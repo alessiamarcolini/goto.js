aa11fcba2de169719b06a908977ebab0eb4eb8c6
thomas <thomas.reolon@studenti.unitn.it>
2019-12-03 18:59:22 +0100

    added corret ending to http requests

--- app/src/routes/stats.js
+++ app/src/routes/stats.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-
 const StatService = require('@app/services/stats');
+const auth = require('middleware/auth');
 
 const route = Router();
 
@@ -16,45 +16,52 @@ module.exports = async function(routes) {
 	routes.use('/users', route);
 
 
-	//ritorna tutte le statistiche
+	//middleware to check if a user exists (implementato da moreno)
+	//routes.use('/:id', auth.check(req))
+
+	//returns every stats
 	route.get('/:id/stats/', async (req, res) => {
 		StatService.getStats(req.params.id)
 			.then((stats) => {res.status(200).json(stats);})
 			.catch((err)=> {
-				res.status(err.status||500)
-				   .json(errMessage(err));
+				res.status(400)
+				   .json(errMessage(err))
+				   .end();
 			});
 	});
 
-	//return stats on daily intake of calories
+	//returns stats on daily intake of calories
 	route.get('/:id/stats/calories', async (req, res) => {
 		StatService.todayCalories(req.params.id)
 			.then((stats) => {res.status(200).json(stats);})
 			.catch((err)=> {
-				res.status(err.status||500)
-				   .json(errMessage(err));
+				res.status(400)
+				   .json(errMessage(err))
+				   .end();
 			});
 	});
 
 
-	//return how much you have drunk today
+	//returns how much you have drunk today
 	route.get('/:id/stats/water', async (req, res) => {
 		StatService.todayWater(req.params.id)
 			.then((stats) => {res.status(200).json(stats);})
 			.catch((err)=> {
-				res.status(err.status||500)
-				   .json(errMessage(err));
+				res.status(400)
+				   .json(errMessage(err))
+				   .end();
 			});
 	});
 
 
-	//return stats on your weight
+	//returns stats on your weight
 	route.get('/:id/stats/weight', async (req, res) => {
 		StatService.weightStats(req.params.id)
 			.then((stats) => {res.status(200).json(stats);})
 			.catch((err)=> {
-				res.status(err.status||500)
-				   .json(errMessage(err));
+				res.status(400)
+				   .json(errMessage(err))
+				   .end();
 			});
 	});
 
